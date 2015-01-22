d5e3537fb19049d6379e6d92c2965ad584375799
aalmiray <aalmiray@gmail.com>
2015-01-22 12:02:39 +0100

    Miscellaneous updates

--- subprojects/griffon-datasource-core/src/main/java/org/codehaus/griffon/runtime/datasource/DataSourceAddon.java
+++ subprojects/griffon-datasource-core/src/main/java/org/codehaus/griffon/runtime/datasource/DataSourceAddon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors.
+ * Copyright 2014-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ public class DataSourceAddon extends AbstractGriffonAddon {
     @Inject
     private DataSourceFactory dataSourceFactory;
 
-    public void onBootstrapEnd(@Nonnull GriffonApplication application) {
+    public void onStartupStart(@Nonnull GriffonApplication application) {
         for (String dataSourceName : dataSourceFactory.getDataSourceNames()) {
             Map<String, Object> config = dataSourceFactory.getConfigurationFor(dataSourceName);
             if (getConfigValueAsBoolean(config, "connect_on_startup", false)) {
